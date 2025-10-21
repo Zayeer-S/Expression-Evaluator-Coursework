384cb34ede06083bfb91f03613db3ba34ccb45a5
A-abd <65906361+A-abd@users.noreply.github.com>
2025-10-21 14:14:54 +0100

    feat: create main function

--- src/expression_evaluator/Program.cs
+++ src/expression_evaluator/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        var precedenceMap = Constants.PrecedenceMap();
+
+        List<string> normalizedTokens;
+        while (true)
+        {
+            var expression = Parser.GetExpression();
+            var tokens = Tokenizer.GetTokens(expression);
+
+            if (TokenValidator.ValidateTokens(tokens, precedenceMap))
+            {
+                normalizedTokens = NormalizeTokens.Normalize(tokens);
+                break;
+            }
+        }
+
+        var postfix = ShuntingYard.InfixToPostfix(normalizedTokens, precedenceMap);
+        var tree = ExpressionTree.BuildTree(postfix, precedenceMap, out var variableNames);
+
+        do
+        {
+            var variables = VariableInputParser.GetVariableValues(variableNames);
+            var result = Evaluator.Evaluate(tree, variables);
+            OutputFormatter.PrintResult(result);
+        }
+        while (VariableInputParser.PromptForReEvaluation());
+    }
+}
